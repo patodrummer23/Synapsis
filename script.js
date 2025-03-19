60a0ccd62416df384399a40b2ec716c441fa414d
Patricio Tajes <patriciont1994@gmail.com>
2025-03-19 16:54:25 -0300

    javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+let palabras = ["auto","pera","azul"];
+console.log(palabras);
+console.log(palabras[2]);
+let boleanos = [true,false,true,false];
+console.log(boleanos);
+let matriz = [
+    [1,2,3],
+    
+]
+
+
+
+
+
+
+
+
